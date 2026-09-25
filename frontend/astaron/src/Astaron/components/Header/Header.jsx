f350f6c79dd67ab44a7736d6f117d44a695db0e7
AhmedNovaChrono <63499400+ahmedNovachrono@users.noreply.github.com>
2026-09-25 18:45:23 +0300

    Update Header.jsx

--- frontend/astaron/src/Astaron/components/Header/Header.jsx
+++ frontend/astaron/src/Astaron/components/Header/Header.jsx
@@ -15,7 +15,7 @@ export const Header = () => {
                 <div className='rounded-4' style={{width: "50px", height: "50px", backgroundColor: "black", boxShadow: "0px 0px 12px 1px #39FF14"}}><AstarothLogo /></div>
                 <h2 className='ms-3 mt-1 font-weight-bolder'>Astaron Learn</h2>
                 </div>
-                <Button style={{background: "none", border: "none", color: "gray", fontWeight: "500"}}><FontAwesomeIcon  icon={faArrowRightFromBracket}/> Log out</Button>
+                <Button style={{background: "none", border: "none", color: "gray", fontWeight: "500"}}><FontAwesomeIcon  icon={faArrowRightFromBracket}/> Log Out</Button>
             </Col>
             <hr className='mt-1' />
         </Row>
